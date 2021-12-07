26fab08bd2480ffd1025ce1526404d1d9710bd1a
gomding <backchida@gmail.com>
2021-12-07 22:50:50 +0900

    feat: 클래스 선언과 상속하는 방법 학습

---
+++ src/main/kotlin/Rectangle.kt
@@ -0,0 +1,10 @@
+// 클래스의 속성은 선언 또는 본문에 나열될 수 있습니다.
+class Rectangle(var height: Double, var length: Double) {
+    var perimeter = (height + length) * 2
+}
+
+fun main() {
+    // 클래스 선언에 나열된 매개 변수를 가진 기본 생성자를 자동으로 활용할 수 있습니다.
+    val rectangle = Rectangle(5.0, 2.0)
+    println("The Perimeter is ${rectangle.perimeter}")
+}
---
+++ src/main/kotlin/Shape.kt
@@ -0,0 +1,3 @@
+// class 클래스를 적용하기 위한 키워드
+class Shape {
+}
